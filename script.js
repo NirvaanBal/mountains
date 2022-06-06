541fa9d76e565660c3f5eac076d5ffdb0ed1daa4
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-06-06 10:32:44 +0530

    Create navigation for schedules

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const header = document.querySelector('header');
 const sections = document.querySelectorAll('section');
 const footer = document.querySelector('footer');
+const mountainNavItems = document.querySelectorAll('.climb nav li');
+// const mountain1 = document.getElementById('m1')
+// const mountain2=
 
 function observation(style, options) {
   return new IntersectionObserver((entries) => {
@@ -19,3 +22,21 @@ sections.forEach((section) => {
 });
 
 observation('reveal', { threshold: 0.5 }).observe(footer);
+
+mountainNavItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const mountainToShow = e.target.dataset.id === 'm1' ? 'm1' : 'm2';
+    const mountainToHide = e.target.dataset.id === 'm1' ? 'm2' : 'm1';
+
+    document.getElementById(mountainToShow).classList.remove('hide');
+    document.getElementById(mountainToHide).classList.add('hide');
+
+    if (mountainToShow === 'm1') {
+      document.querySelector(`li[data-id='m1']`).classList.add('active');
+      document.querySelector(`li[data-id='m2']`).classList.remove('active');
+    } else {
+      document.querySelector(`li[data-id='m2']`).classList.add('active');
+      document.querySelector(`li[data-id='m1']`).classList.remove('active');
+    }
+  });
+});
